98c65ea0c03ed8df5d9a76771c47a09fe5ff9c23
Vanessa Soares <nessa.soares46@gmail.com>
2019-11-10 23:32:54 -0300

    designer das telas

--- Views/Home.php
+++ Views/Home.php
@@ -1,20 +1,51 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+      <link rel="stylesheet" type="text/css" href="Styles/Home.css">
+      <!-- Última versão CSS compilada e minificada -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
         <title></title>
     </head>
+    <script>
+    
+    </script>
     <body>
-        <form action="Pesquisador/Cadastro.php">
-            <input type="submit" value="Cadastrar">
-        </form>
-        <form action="Pesquisador/Login.php">
-            <input type="submit" value="Login">
-        </form>
-        <br>
-        <form action="">
-            <input type="text" placeholder="Codigo do teste...">
-            <input type="submit" value="Procurar">
-        </form>
+<nav class="navbar navbar-default navbar-fixed-top navbar-transparent " role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Pesquisas</a>
+    </div>
+
+    <!-- nav links, forms, -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="Pesquisador/Cadastro.php">Cadastrar</a></li>
+        <li><a href="Pesquisador/Login.php" data-toggle="modal" class="btn btn-primary" id="nav-btn"> Entrar </a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<section id="pesquisa">
+  <div class="container">
+    <div class="content-center">
+      <h1 class="margintop-lg">Encontre sua Pesquisa</h1>
+        <form class="form-inline">
+          <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+          </form>
+        </div>
+      </div>
+    </section>
+
     </body>
 </html>
--- Views/Pesquisador/Cadastro.php
+++ Views/Pesquisador/Cadastro.php
@@ -1,15 +1,57 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <title></title>
+    <link rel="stylesheet" type="text/css" href="../Styles/Home.css">
+  <!-- Última versão CSS compilada e minificada -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     </head>
     <body>
-        <form action="../../Controllers/Pesquisador/Cadastrar.php" method="post">
-            CADASTRO<br>
-            Nome: <input type="text" name="nome"><br>
-            Senha: <input type="text" name="senha"><br>
-            <input type="submit" value="Ok">
-        </form>
+    <section id="cadastro" method = "post">
+        <div class="container-cadastro">
+            <div class="row">
+            <div id = coluna>
+                <div class="col-md-6 margintop-sm">
+                    <h3>Crie as melhores pesquisas <b>Encontre aqui</b></h3>
+                    <p></p>
+                </div> 
+            </div>
+                <div class="col-md-6 margintop-sm">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id placeholder="Nome">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id placeholder="Sobre Nome">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <input type="email" class="form-control" id placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id placeholder="Senha">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id placeholder="Confirmar Senha">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <a href="../../Controllers/Pesquisador/Cadastrar.php" class="btn btn-primary full-width">Cadastrar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     </body>
 </html>
